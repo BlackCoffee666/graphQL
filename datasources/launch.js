313fe393e412be38e7228baa16b3106752a472f1
The Crooked Man <blackcoffee666@bk.ru>
2020-05-15 16:45:55 +0300

    graph resolvers

--- datasources/launch.js
+++ datasources/launch.js
@@ -1,18 +1,12 @@
-const { RESTDataSource } = require('apollo-datasource-rest');
+const { RESTDataSource } = require("apollo-datasource-rest");
 
 class LaunchAPI extends RESTDataSource {
-  constructor() {
-    super();
-    this.baseURL = 'https://api.spacexdata.com/v2/';
-  }
-}
-async getAllLaunches() {
-    const response = await this.get('launches');
+  async getAllLaunches() {
+    const response = await this.get("launches");
     return Array.isArray(response)
       ? response.map(launch => this.launchReducer(launch))
       : [];
   }
-  module.exports = LaunchAPI;
   launchReducer(launch) {
     return {
       id: launch.flight_number || 0,
@@ -21,22 +15,28 @@ async getAllLaunches() {
       mission: {
         name: launch.mission_name,
         missionPatchSmall: launch.links.mission_patch_small,
-        missionPatchLarge: launch.links.mission_patch,
+        missionPatchLarge: launch.links.mission_patch
       },
       rocket: {
         id: launch.rocket.rocket_id,
         name: launch.rocket.rocket_name,
-        type: launch.rocket.rocket_type,
-      },
+        type: launch.rocket.rocket_type
+      }
     };
   }
   async getLaunchById({ launchId }) {
-    const response = await this.get('launches', { flight_number: launchId });
+    const response = await this.get("launches", { flight_number: launchId });
     return this.launchReducer(response[0]);
   }
-  
   getLaunchesByIds({ launchIds }) {
     return Promise.all(
-      launchIds.map(launchId => this.getLaunchById({ launchId })),
+      launchIds.map(launchId => this.getLaunchById({ launchId }))
     );
   }
+  constructor() {
+    super();
+    this.baseURL = "https://api.spacexdata.com/v2/";
+  }
+}
+
+module.exports = LaunchAPI;
